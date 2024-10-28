ad1406633de04b6437659637b4e0778738653f8e
Mohamed Ganaway <157913115+Mohamed-Ganaway@users.noreply.github.com>
2024-10-28 14:00:15 +0200

    refresh token out of the middleware

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,13 @@ use App\Http\Controllers\FavoriteMealController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('refresh', [AuthController::class, 'refresh']);
 
 Route::middleware('auth:api')->group(function(){
 
 
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('user', [AuthController::class, 'user']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
 
     
  
